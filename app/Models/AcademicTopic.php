6b4616b06fac6baa303498c9321ade0f6ed3bd2f
Isaac Adzah Sai <isaacsai030@gmail.com>
2023-08-08 00:33:52 +0000

    Track all model changes

--- app/Models/AcademicTopic.php
+++ app/Models/AcademicTopic.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use App\Traits\Trackable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class AcademicTopic extends Model
 {
     use HasFactory;
+    use Trackable;
 
     /**
      * @var array<int, string>
