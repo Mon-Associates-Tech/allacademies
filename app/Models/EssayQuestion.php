e7e85f919498bb5097be20b51d7078cea3a58e49
Isaac Adzah Sai <isaacsai030@gmail.com>
2023-08-08 01:00:27 +0000

    Add soft delete to 7 tables

--- app/Models/EssayQuestion.php
+++ app/Models/EssayQuestion.php
@@ -5,10 +5,12 @@ namespace App\Models;
 use App\Support\Mark;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class EssayQuestion extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     /**
      * @var array<int, string>
