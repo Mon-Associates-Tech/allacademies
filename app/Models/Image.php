b0757f1ce3e7ad2012048516c4cf35582b19d2db
Warihana Gumah <warihana.gumah@thegitlab.com>
2023-08-10 14:22:51 +0000

    Model and migration for image upload

---
+++ app/Models/Image.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class QuestionImages extends Model
+{
+    use HasFactory;
+
+    /**
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'image_url',
+        'description',
+        'tags',
+    ];
+
+    
+}
