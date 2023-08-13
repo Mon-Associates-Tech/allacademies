f69c25439bd1248a4552c80fd894cc779199885b
Isaac Adzah Sai <isaacsai030@gmail.com>
2023-08-13 16:59:47 +0000

    Add mailgun driver

--- resources/views/branding.blade.php
+++ resources/views/branding.blade.php
@@ -68,9 +68,9 @@
       </div>
       <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
         <div class="hidden sm:mb-8 sm:flex sm:justify-center">
-          {{-- <div class="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-            Announcing our next round of funding. <a href="#" class="font-semibold text-primary-600"><span class="absolute inset-0" aria-hidden="true"></span>Read more <span aria-hidden="true">&rarr;</span></a>
-          </div> --}}
+          <div class="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+            Announcing our interactive online quiz. <a href="{{ route('sign-in') }}" class="font-semibold text-primary-600"><span class="absolute inset-0" aria-hidden="true"></span>Try it <span aria-hidden="true">&rarr;</span></a>
+          </div>
         </div>
         <div class="text-center">
           <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Learn and assess yourself online</h1>
