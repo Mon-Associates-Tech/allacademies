7837ea6cc596d2291452d67a241ad3a560e5ebd4
Warihana Gumah <warihana.gumah@thegitlab.com>
2023-11-07 22:34:02 +0000

    score and difficulty level made a component

---
+++ resources/views/components/question-attributes.blade.php
@@ -0,0 +1,12 @@
+@props(['score'])
+<div>
+    <x-form.select name="difficulty_level" label="Difficulty Level" :options="[
+        'unspecified' => 'Unspecified',
+        'easy' => 'Easy',
+        'medium' => 'Medium',
+        'difficult' => 'Difficult',
+    ]" />
+</div>
+<div>
+    <x-form.input name="score" type="number" value="{{ $score }}" />
+</div>
