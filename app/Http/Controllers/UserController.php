c1b362c684433c2ed5c58167e2c3c6ad094e7a1c
Isaac Adzah Sai <isaacsai030@gmail.com>
2024-05-08 16:43:10 +0000

    Hide fake users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,20 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    
+
     /**
      * Display a listing of the resource(user).
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
         $this->authorize('administrate');
 
-        $users = User::query()->latest('id')->paginate();
+        $users = User::query()->when($request->missing('all'))->whereNotNull('email_verified_at')->latest('id')->paginate()->withQueryString();
 
         return view('users.index', [
             'users' => $users,
