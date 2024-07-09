d385eaf1c69f44fa0b4c4af769072b91bc847068
Isaac Adzah Sai <isaacsai030@gmail.com>
2024-07-09 13:28:08 +0000

    Fire

--- resources/views/branding.blade.php
+++ resources/views/branding.blade.php
@@ -73,7 +73,7 @@
           </div>
         </div>
         <div class="text-center">
-          <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Study and assess yourself online</h1>
+          <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Study and assess yourself online 🔥</h1>
           <p class="mt-6 text-lg leading-8 text-gray-600">Study and move at your own pace without any pressure. Easily assess yourself to enhance your understanding. Learning made easier and flexible.</p>
           <div class="mt-10 flex items-center justify-center gap-x-6">
             <a href="{{ route('sign-up') }}" class="rounded-md bg-primary-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600">Get started</a>
