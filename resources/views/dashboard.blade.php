b31ae128f6467a5a672a709b8f79b7c2c23628ae
Ekow Sackey <120583131+SS-ekow@users.noreply.github.com>
2024-10-21 14:57:29 +0000

    made changes to dashboard page and navbar

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,10 +1,64 @@
 <x-auth title="Dashboard">
     @if ($academicSubjects->count())
+
+    <section class="w-full h-60 mb-8 grid grid-rows-2 gap-4 md:grid md:grid-cols-[30%_68%] md:h-40">
+        <div class="flex flex-col justify-between text-center items-center rounded-3xl bg-blue-300 p-4 md:h-40">
+            <h3 class="text-2xl font-semibold">Hello User</h3>
+            <p>Welcome to your Dashboard</p>
+            <div class="btn bg-range drop-shadow-2xl px-4 py-2 rounded-full w-1/2 text-sm text-white md:w-full">Explore more courses</div>
+        </div>
+        <div class="grid grid-cols-[30%_68%] gap-4 items-center text-center bg-blue-300  rounded-3xl p-4 md:h-40">
+            <div>
+                Notifications
+            </div>
+            <div class="flex flex-col md:gap-4 pb-1 items-center text-center bg-white rounded-2xl">
+
+                <h3>Toolbox</h3>
+                <div class="flex w-full justify-between">
+    
+                    <div class="border-r w-full flex flex-col items-center">
+                        <img src="{{ asset('img/deadline.gif') }}" alt="animation" class='w-10 h-10 bg-inherit'>
+                        <p class="text-sm">Calendar</p>
+                    </div>
+                    <div class="border-r w-full flex flex-col items-center">
+                        <img src="{{ asset('img/bank.gif') }}" alt="animation" class='w-10 h-10 bg-inherit'>
+                        <p class="text-sm">Test Bank</p>
+                    </div>
+                    <div class=" w-full flex flex-col items-center">
+                        <img src="{{ asset('img/history.png') }}" alt="animation" class='w-10 h-10 bg-inherit'>
+                        <p class="text-sm">Recent</p>
+                    </div>
+                    
+                    
+                </div>
+            </div>
+
+
+        </div>
+
+    </section>
+
+    <section class="mt-8">
+
+    
+
+    <h3 class="font-semibold text-2xl">My Courses</h3>
+    <nav class="w-full mb-4 py-2">
+        <ul class="flex justify-between items-center">
+            <li><a href="/">University</a></li>
+            <li><a href="/">Senior High</a></li>
+            <li><a href="/">Junior High</a></li>
+            <li><a href="/">Primary </a></li>
+            <li><a href="/">PreSchool</a></li>
+            
+            
+        </ul>
+    </nav>
   <x-table>
       <x-slot name="head">
           <tr>
               <x-table.th>Available Subjects</x-table.th>
-              <x-table.th><span class="sr-only">Actions</span></x-table.th>
+              <x-table.th><span >Actions</span></x-table.th>
           </tr>
       </x-slot>
 
@@ -40,4 +94,6 @@
   </a>
 
   @endif
+
+  </section>
 </x-auth>
