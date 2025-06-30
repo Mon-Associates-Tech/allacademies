f52cd197d21421892b5285637e4b61c235154311
Promise <promisepaps@gmail.com>
2025-06-30 09:00:36 +0000

    book subscription modal

--- resources/views/livewire/students/book-subscription-modal.blade.php
+++ resources/views/livewire/students/book-subscription-modal.blade.php
@@ -187,7 +187,7 @@
                                         class="px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md hover:bg-gray-50 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-600 dark:hover:bg-gray-600">
                                     Close
                                 </button>
-                                <a href="/dashboard?activeTab=my-books&bookTab=subscriptions"
+                                <a href="{{route('subscriptions.index')}}"
                                    class="px-4 py-2 text-sm font-medium text-white bg-blue-600 border border-transparent rounded-md hover:bg-blue-700">
                                     View My Subscriptions
                                 </a>
