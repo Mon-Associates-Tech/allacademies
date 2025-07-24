64a1aa94c566b2eb90e47a61a0d3e2013faa7eac
Promise <promisepaps@gmail.com>
2025-07-24 18:54:07 +0000

    quiz - show create button

--- resources/views/quizzes/index.blade.php
+++ resources/views/quizzes/index.blade.php
@@ -1,39 +1,44 @@
-<x-layouts.app title="Quizzes" :has-action="false">
+<x-layouts.app app-name="Quizzes" :has-action="false">
     <x-slot name="breadcrumb">
-        <x-breadcrumb />
+        <x-breadcrumb/>
     </x-slot>
     @can('privileged', $currentTeam)
-        <x-slot name="action">
-            <x-link.primary :to="route('quizzes.create', ['academic_subject' => $academicSubject, 'academic_level' => getRouteParameter('academic_level'), 'academic_group' => getRouteParameter('academic_group')])">New Quiz</x-link.primary>
-        </x-slot>
+        <div class="text-right mb-4">
+            <x-link.primary
+                :to="route('quizzes.create', ['academic_subject' => $academicSubject, 'academic_level' => getRouteParameter('academic_level'), 'academic_group' => getRouteParameter('academic_group')])">
+                New Quiz
+            </x-link.primary>
+        </div>
     @endcan
 
     @if ($quizzes->count())
-    <x-table>
-        <x-slot name="head">
-            <tr>
-                <x-table.th>Title</x-table.th>
-                <x-table.th><span class="sr-only">Actions</span></x-table.th>
-            </tr>
-        </x-slot>
+        <x-table>
+            <x-slot name="head">
+                <tr>
+                    <x-table.th>Title</x-table.th>
+                    <x-table.th><span class="sr-only">Actions</span></x-table.th>
+                </tr>
+            </x-slot>
 
-        @foreach ($quizzes as $quiz)
-            <tr>
-                <x-table.td bold>{{ $quiz->title }}</x-table.td>
-                <x-table.td action>
-                    <a class="text-primary-600 hover:text-primary-900" href="{{ route('quizzes.start', ['academic_subject' => $academicSubject, 'academic_level' => getRouteParameter('academic_level'), 'academic_group' => getRouteParameter('academic_group'), 'quiz' => $quiz]) }}">Quiz</a>
-                    @can('privileged', $currentTeam)
-                        <a class="text-primary-600 hover:text-primary-900" href="{{ route('quizzes.scores', ['academic_subject' => $academicSubject, 'academic_level' => getRouteParameter('academic_level'), 'academic_group' => getRouteParameter('academic_group'), 'quiz' => $quiz]) }}">Scores</a>
-                    @endcan
-                </x-table.td>
-            </tr>
-        @endforeach
-    </x-table>
+            @foreach ($quizzes as $quiz)
+                <tr>
+                    <x-table.td bold>{{ $quiz->title }}</x-table.td>
+                    <x-table.td action>
+                        <a class="text-primary-600 hover:text-primary-900"
+                           href="{{ route('quizzes.start', ['academic_subject' => $academicSubject, 'academic_level' => getRouteParameter('academic_level'), 'academic_group' => getRouteParameter('academic_group'), 'quiz' => $quiz]) }}">Quiz</a>
+                        @can('privileged', $currentTeam)
+                            <a class="text-primary-600 hover:text-primary-900"
+                               href="{{ route('quizzes.scores', ['academic_subject' => $academicSubject, 'academic_level' => getRouteParameter('academic_level'), 'academic_group' => getRouteParameter('academic_group'), 'quiz' => $quiz]) }}">Scores</a>
+                        @endcan
+                    </x-table.td>
+                </tr>
+            @endforeach
+        </x-table>
 
-    <div class="mt-3">
-        {{ $quizzes->links() }}
-    </div>
+        <div class="mt-3">
+            {{ $quizzes->links() }}
+        </div>
     @else
-    <x-blank />
+        <x-blank/>
     @endif
 </x-layouts.app>
