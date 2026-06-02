d24aaf4af63aa7a15c3e07435554533dd554cd09
Promise <promisepaps@gmail.com>
2026-06-02 15:00:18 +0000

    comment echo

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -38,7 +38,7 @@ window.DOMPurify = DOMPurify;
 // resources/js/app.js (or a dedicated entry point)
 import './proctoring'; // If proctoring.js is a module that attaches to window
 import './exam-heartbeat'; // If exam-heartbeat.js is a module that attaches to window
-import './echo';
+//import './echo';
 // Register Chart.js components (added RadialLinearScale for radar charts)
 Chart.register(CategoryScale, LinearScale, BarElement, LineElement, PointElement, ArcElement, Title, Tooltip, Legend, RadialLinearScale);
 
